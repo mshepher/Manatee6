4d3ebac3e9090d9162496dd2d05ae2922b2ee8c8
Meredith Shepherd <mshepher@akamai.com>
2019-03-04 15:49:33 -0500

    Add AppCenter.Crashes SDK

--- Manatee7/App.xaml.cs
+++ Manatee7/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Manatee7
@@ -16,6 +18,8 @@ namespace Manatee7
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=a85016c7-bd85-42eb-a9eb-ba2cd88caea8;",
+                 typeof(Crashes));
             // Handle when your app starts
         }
 
